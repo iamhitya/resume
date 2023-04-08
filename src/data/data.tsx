6f8d8e570cb79d754557533513cb08d9f6d9c867
hkvaghasiya <hkvaghasiya@intelionsys.com>
2023-04-08 08:59:14 +0530

    Chore: Update Meta Title and Description

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -43,8 +43,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'Hiteshkumar Vaghasiya',
-  description: "Hiteshkumar's resume",
+  title: 'Hiteshkumar - Resume',
+  description: "Welcome to my personal resume website! Here you can learn more about my skills, experience, and qualifications. Take a look and feel free to reach out if you have any questions or opportunities to collaborate. I look forward to connecting with you.",
 };
 
 /**
