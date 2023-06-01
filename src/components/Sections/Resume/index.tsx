e5fba2573c1be80a6957351579415063e57648f9
hkvaghasiya <hkvaghasiya@intelionsys.com>
2023-06-01 12:59:54 +0530

    Chore: Update resume section sequence

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -10,16 +10,6 @@ const Resume: FC = memo(() => {
   return (
     <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
       <div className="flex flex-col divide-y-2 divide-neutral-300">
-        <ResumeSection title="Education">
-          {education.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection>
-        <ResumeSection title="Work Experience">
-          {experience.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection>
         <ResumeSection title="Skills">
           <p className="pb-8">My software and application expertise is very diverse and includes <b>.NET Framework, C#, MVC, .NET Core, Web API, MS SQL Server, MongoDB, PostgreSQL, Entity Framework, LINQ, Angular, Vue.js, <a href='https://www.credly.com/users/iamhitya/badges' target='_blank' className="text-blue-600">Azure</a>, HTML5, JavaScript, JQuery, CSS, and nopCommerce</b>.</p>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
@@ -27,6 +17,16 @@ const Resume: FC = memo(() => {
               <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
             ))}
           </div>
+        </ResumeSection>
+        <ResumeSection title="Work Experience">
+          {experience.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </ResumeSection>
+        <ResumeSection title="Education">
+          {education.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
         </ResumeSection>
       </div>
     </Section>
